11528248b7f997307890d36d4703c0178253f4d2
Emadenni <emanyele.dng86@gmail.com>
2024-07-08 00:42:10 +0200

    endpoint /register implemented succesfully

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+const cors = require("cors");
 dotenv.config();
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-app.use(cors());
+
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB', err);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Here i am");
 });
 
-app.get('/', (req, res) => {
-  res.send('Here i am');
-});
-
+app.use(cors());
+app.use(bodyParser.json());
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use ("/api", userRoutes)
--- frontend/sqramb/src/components/RegistrationForm/RegistrationForm.tsx
+++ frontend/sqramb/src/components/RegistrationForm/RegistrationForm.tsx
@@ -3,7 +3,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import "./registrationForm.scss";
 import TermsAndConditions from "../TermsAndConditions/TermsAndConditions";
-import countries from "react-select-country-list"; 
+import countries from "react-select-country-list";
 
 interface FormState {
   username: string;
@@ -20,7 +20,6 @@ interface FormState {
   terms: boolean;
 }
 
-
 const initialFormState: FormState = {
   username: "",
   email: "",
@@ -32,7 +31,7 @@ const initialFormState: FormState = {
   gender: "",
   city: "",
   country: "",
-  state: "", 
+  state: "",
   terms: false,
 };
 
@@ -45,7 +44,7 @@ const validationSchema = Yup.object({
     .oneOf([Yup.ref("password")], "Passwords must match")
     .required("Required"),
   fullName: Yup.string().required("Required"),
-  phoneNumber: Yup.string().required("Required"),
+  phoneNumber: Yup.string(),
   dob: Yup.string().required("Required"),
   gender: Yup.string().required("Required"),
   city: Yup.string().required("Required"),
@@ -56,7 +55,6 @@ const validationSchema = Yup.object({
 
 const RegistrationForm: React.FC = () => {
   const [isDateInputVisible, setIsDateInputVisible] = useState(false);
-  const [states, setStates] = useState<string[]>([]); 
   const [isTermsOverlayOpen, setIsTermsOverlayOpen] = useState(false);
 
   const formik = useFormik<FormState>({
@@ -67,8 +65,6 @@ const RegistrationForm: React.FC = () => {
       formik.resetForm();
     },
   });
-
-
 
   const handleDateFocus = () => {
     setIsDateInputVisible(true);
@@ -96,157 +92,158 @@ const RegistrationForm: React.FC = () => {
 
   return (
     <>
-    <form className="reg-form" onSubmit={formik.handleSubmit}>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        value={formik.values.username}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Username"
-      />
-      {formik.touched.username && formik.errors.username ? <div>{formik.errors.username}</div> : null}
-
-      <input
-        type="email"
-        id="email"
-        name="email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="E-mail"
-      />
-      {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
-
-      <input
-        type="password"
-        id="password"
-        name="password"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Password"
-      />
-      {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
-
-      <input
-        type="password"
-        id="confirmPassword"
-        name="confirmPassword"
-        value={formik.values.confirmPassword}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Confirm Password"
-      />
-      {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-        <div>{formik.errors.confirmPassword}</div>
-      ) : null}
-
-      <input
-        type="text"
-        id="fullName"
-        name="fullName"
-        value={formik.values.fullName}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Full Name"
-      />
-      {formik.touched.fullName && formik.errors.fullName ? <div>{formik.errors.fullName}</div> : null}
-
-      <input
-        type="tel"
-        id="phoneNumber"
-        name="phoneNumber"
-        value={formik.values.phoneNumber}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Phone Number"
-      />
-      {formik.touched.phoneNumber && formik.errors.phoneNumber ? <div>{formik.errors.phoneNumber}</div> : null}
-
-      <div className="date-input-wrapper">
-        {!isDateInputVisible && (
-          <input type="text" placeholder="Date of Birth" onFocus={handleDateFocus} className="placeholder-input" />
-        )}
-        {isDateInputVisible && (
-          <input
-            type="date"
-            id="dob"
-            name="dob"
-            value={formik.values.dob}
-            onChange={formik.handleChange}
-            onBlur={handleDateBlur}
-          />
-        )}
-        {formik.touched.dob && formik.errors.dob ? <div>{formik.errors.dob}</div> : null}
-      </div>
-
-      <select
-        id="gender"
-        name="gender"
-        value={formik.values.gender}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      >
-        <option value="">Select Gender</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="other">Other</option>
-        <option value="prefer-not-to-say">Prefer not to say</option>
-      </select>
-      {formik.touched.gender && formik.errors.gender ? <div>{formik.errors.gender}</div> : null}
-
-      <input
-        type="text"
-        id="city"
-        name="city"
-        value={formik.values.city}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="City"
-      />
-      {formik.touched.city && formik.errors.city ? <div className="error-message">{formik.errors.city}</div> : null}
-
-      <select
-        id="country"
-        name="country"
-        value={formik.values.country}
-        onChange={handleCountryChange}
-        onBlur={formik.handleBlur}
-      >
-        <option value="">Select Country</option>
-        {countries()
-          .getData()
-          .map((country: any) => (
-            <option key={country.label} value={country.label}>
-              {country.label}
-            </option>
-          ))}
-      </select>
-      {formik.touched.country && formik.errors.country ? (
-        <div className="error-message">{formik.errors.country}</div>
-      ) : null}
-
-      <div className="reg-form__terms-box">
+      <form className="reg-form" onSubmit={formik.handleSubmit}>
         <input
-          type="checkbox"
-          id="terms"
-          name="terms"
-          checked={formik.values.terms}
+          type="text"
+          id="username"
+          name="username"
+          value={formik.values.username}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
+          placeholder="Username"
         />
-        <label htmlFor="terms">I agree to the Terms and Conditions</label>
-      </div>
-      {formik.touched.terms && formik.errors.terms ? <div>{formik.errors.terms}</div> : null}
+        {formik.touched.username && formik.errors.username ? <div>{formik.errors.username}</div> : null}
 
-      <p onClick={openTermsOverlay} className="terms-par">Read Terms And Conditions</p>
-      <button type="submit">Register</button>
-      
-    </form>
-  <TermsAndConditions isOpen={isTermsOverlayOpen} onClose={closeTermsOverlay}/>   
-  </>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="E-mail"
+        />
+        {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Password"
+        />
+        {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
+
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Confirm Password"
+        />
+        {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+          <div>{formik.errors.confirmPassword}</div>
+        ) : null}
+
+        <input
+          type="text"
+          id="fullName"
+          name="fullName"
+          value={formik.values.fullName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Full Name"
+        />
+        {formik.touched.fullName && formik.errors.fullName ? <div>{formik.errors.fullName}</div> : null}
+
+        <input
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={formik.values.phoneNumber}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Phone Number"
+        />
+        {formik.touched.phoneNumber && formik.errors.phoneNumber ? <div>{formik.errors.phoneNumber}</div> : null}
+
+        <div className="date-input-wrapper">
+          {!isDateInputVisible && (
+            <input type="text" placeholder="Date of Birth" onFocus={handleDateFocus} className="placeholder-input" />
+          )}
+          {isDateInputVisible && (
+            <input
+              type="date"
+              id="dob"
+              name="dob"
+              value={formik.values.dob}
+              onChange={formik.handleChange}
+              onBlur={handleDateBlur}
+            />
+          )}
+          {formik.touched.dob && formik.errors.dob ? <div>{formik.errors.dob}</div> : null}
+        </div>
+
+        <select
+          id="gender"
+          name="gender"
+          value={formik.values.gender}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        >
+          <option value="">Select Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+          <option value="prefer-not-to-say">Prefer not to say</option>
+        </select>
+        {formik.touched.gender && formik.errors.gender ? <div>{formik.errors.gender}</div> : null}
+
+        <input
+          type="text"
+          id="city"
+          name="city"
+          value={formik.values.city}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="City"
+        />
+        {formik.touched.city && formik.errors.city ? <div className="error-message">{formik.errors.city}</div> : null}
+
+        <select
+          id="country"
+          name="country"
+          value={formik.values.country}
+          onChange={handleCountryChange}
+          onBlur={formik.handleBlur}
+        >
+          <option value="">Select Country</option>
+          {countries()
+            .getData()
+            .map((country: any) => (
+              <option key={country.label} value={country.label}>
+                {country.label}
+              </option>
+            ))}
+        </select>
+        {formik.touched.country && formik.errors.country ? (
+          <div className="error-message">{formik.errors.country}</div>
+        ) : null}
+
+        <div className="reg-form__terms-box">
+          <input
+            type="checkbox"
+            id="terms"
+            name="terms"
+            checked={formik.values.terms}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <label htmlFor="terms">I agree to the Terms and Conditions</label>
+        </div>
+        {formik.touched.terms && formik.errors.terms ? <div>{formik.errors.terms}</div> : null}
+
+        <p onClick={openTermsOverlay} className="terms-par">
+          Read Terms And Conditions
+        </p>
+        <button type="submit">Register</button>
+      </form>
+      <TermsAndConditions isOpen={isTermsOverlayOpen} onClose={closeTermsOverlay} />
+    </>
   );
 };
 
